c58e8273a8bb52f25bfbf2496c46f140b0e6866c
Jakub.Nowak <jakub.nowak@pspg.pl>
2024-01-28 01:41:19 +0100

    Zmiany 2

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj: Login;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.loginObj = new Login();
+  }
+
+  onLogin() {
+    debugger;
+    this.http.post('https://freeapi.miniprojectideas.com/api/User/Login', this.loginObj).subscribe((res:any)=>{
+      if(res.result){
+        alert("login Succes")
+        this.router.navigateByUrl('/dashboard')
+      } else{
+        alert(res.message)
+      }
+    })
+  }
+}
+
+export class Login{
+  EmailId: string;
+  Password: string;
+  constructor() {
+    this.EmailId = '';
+    this.Password = '';
+  }
 
 }
